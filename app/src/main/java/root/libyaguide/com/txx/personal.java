082a0ddd6fb9874212a8a9ca8585072b342756f3
Mohammad Adala <mohammadoo397@gmail.com>
2016-10-30 15:57:13 +0200

    first commit

---
+++ app/src/main/java/root/libyaguide/com/txx/personal.java
@@ -0,0 +1,13 @@
+package root.libyaguide.com.txx;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class personal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.personal);
+    }
+}
---
+++ app/src/main/java/root/libyaguide/com/txx/request1.java
@@ -0,0 +1,13 @@
+package root.libyaguide.com.txx;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class request1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_request1);
+    }
+}
